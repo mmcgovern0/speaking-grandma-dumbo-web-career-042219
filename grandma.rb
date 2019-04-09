e7d0cccddcec3610de87f8f947dc322bc0e19ef7
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-09 17:36:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,8 @@
 def speak_to_grandma(phrase)
  if phrase == phrase.upcase
    return "No, NOT SINCE 1938!"
-   elsif phrase == "I LOVE YOU GRANDMA!"
-   return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
  else 
    return "HUH?! SPEAK UP, SONNY!"
 end
